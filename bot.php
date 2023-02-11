e245ddbdd499961b79c157472d1e8728e68987f4
truncgil <umitreva@gmail.com>
2023-02-11 16:57:24 +0300

    added city name in per result data

--- bot.php
+++ bot.php
@@ -1,13 +1,29 @@
 <pre>
 <?php 
 
-$deprem = file_get_contents("https://api.orhanaydogdu.com.tr/deprem/live.php?limit=10000");
-$deprem = json_decode($deprem,true);
-print_R($deprem);
- ?>
+$depremDatas = file_get_contents("https://api.orhanaydogdu.com.tr/deprem/live.php?limit=10000");
+$depremDatas = json_decode($depremDatas,true);
+$resultDatas = $depremDatas['result'];
+
+$refactoringData = [];
+
+foreach($resultDatas AS $resultData) {
+
+
+    $title = $resultData['title'];
+    preg_match('#\((.*?)\)#', $title, $match);
+
+    
+    $resultData['city'] = $match[1];
+    $refactoringData[] = $resultData;
+    
+}
+print_r($refactoringData);
+  ?>
  </pre>
  <?php 
 
-$result = json_encode($deprem['result'], JSON_UNESCAPED_UNICODE);
+
+$result = json_encode($refactoringData, JSON_UNESCAPED_UNICODE);
 
 file_put_contents("today.json",$result); ?>
