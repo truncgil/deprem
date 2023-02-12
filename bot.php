2c591cd053346a91977f91895532e4249797e7c7
truncgil <umitreva@gmail.com>
2023-02-12 07:23:31 +0300

    Refactoring api on direct rasathane system

--- bot.php
+++ bot.php
@@ -24,31 +24,13 @@ if ( $response === false) {
 
     $refactoringResponse = [];
 
-    /*
-    "title": "YASSIPINAR-ALTINYAYLA (SIVAS)",
-        "date": "2023.02.12 05:36:45",
-        "lokasyon": "YASSIPINAR-ALTINYAYLA (SIVAS)",
-        "lat": 39.3008,
-        "lng": 36.72,
-        "mag": 2.5,
-        "depth": 5,
-        "coordinates": [36.72, 39.3008],
-        "geojson": {
-            "type": "Point",
-            "coordinates": [36.72, 39.3008]
-        },
-        "location_properties": null,
-        "rev": null,
-        "date_stamp": "2023-02-12",
-        "date_day": "2023-02-12",
-        "date_hour": "05:36:45",
-        "timestamp": "1676176605",
-        "location_tz": "Europe/Istanbul"
-    */
+    
     foreach($response AS $line) {
         $line = preg_split('/\s+/', $line);
         
         $title = $line[8] . " " . $line[9];
+        $title = str_replace("İlksel", "", $title);
+        $title = trim($title);
 
         $lat = (double) $line[2];
         $lng = (double) $line[3];
@@ -58,6 +40,7 @@ if ( $response === false) {
         preg_match('#\((.*?)\)#', $title, $match);
         $data = [
             'title' => $title,
+            'type' => $line[10]=='' ? 'İlksel' : $line[10],
             'date' => $date . ' ' . $time,
             'lat' => $lat,
             'lng' => $lng,
@@ -75,11 +58,11 @@ if ( $response === false) {
                 ]
             ],
             'location_properties' => '',
-            'rev' => $line[0],
-            'date_stamp' => $line[0],
-            'date_day' => $line[0],
-            'date_hour' => $line[1],
-            'timestamp' => time(),
+          //  'rev' => $line[0],
+          //  'date_stamp' => $line[0],
+            'date_day' => $date,
+            'date_hour' => $time,
+            'timestamp' => strtotime($date . ' ' . $time),
             'location_tz' => '',
             'city' => $match[1]
         ];
@@ -87,33 +70,19 @@ if ( $response === false) {
       //  var_dump($line);
     }
 
-    var_dump( $refactoringResponse );
-    $depremDatas = json_decode($response,true);
-    $resultDatas = $depremDatas['result'];
-    var_dump($depremDatas);
+    var_dump($refactoringResponse);
 
-    $refactoringData = [];
-
-    foreach($resultDatas AS $resultData) {
-
-
-        $title = $resultData['title'];
-        preg_match('#\((.*?)\)#', $title, $match);
-
-        
-        $resultData['city'] = $match[1];
-        $refactoringData[] = $resultData;
-        
-    }
-    print_r($refactoringData);
+   
       ?>
     </pre>
     <?php 
 
 
-    $result = json_encode($refactoringData, JSON_UNESCAPED_UNICODE);
+    $result = json_encode($refactoringResponse, JSON_UNESCAPED_UNICODE);
+    $last = json_encode($refactoringResponse[0], JSON_UNESCAPED_UNICODE);
 
     file_put_contents("today.json",$result); 
+    file_put_contents("last.json",$last); 
 
 }
 ?>
